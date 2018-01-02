38848405166403b6a9f00a05a65027dc7072feb8
mxx0707 <1592666931@qq.com>
2018-01-02 14:25:31 +0800

    test项目首次提交

---
+++ test1/src/com/test/demo/test.java
@@ -0,0 +1,18 @@
+package com.test.demo;
+
+//测试代码
+public class test {
+    public static void outwirte(){
+        System.out.print("hahaha");
+        System.out.print("hahaha");
+        System.out.print("h啦啦a");
+    }
+    public static void main(String[] args){
+        outwirte();
+        System.out.println("-----------YouMeek.com-----------值=" + "lalal" + "," + "当前类=test.()");
+
+         for(int i =0;i<100;i++) {
+             system.out.print(i);
+         }
+    }
+}
